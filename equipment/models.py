994e7d64cae08ed72cf9f1ec67fc16ef302c2dd5
ERAjunior <onemywowmail@yandex.ru>
2016-02-09 16:30:37 +0300

    Изменил поля в таблице Description

--- equipment/models.py
+++ equipment/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 
 class Area(models.Model):
@@ -11,6 +12,7 @@ class Area(models.Model):
         db_table = 'area'
         verbose_name = u'Район города '
         verbose_name_plural = u'Районы города'
+
     name_area = models.CharField(u'Район города', max_length=200)
     date_create_area = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
@@ -18,12 +20,14 @@ class Area(models.Model):
     def __unicode__(self):
         return self.name_area
 
+
 class Street(models.Model):
     class Meta():
         db_table = 'street'
         verbose_name = u'Улицу'
         verbose_name_plural = u'Улицы'
-    name_street = models.CharField(u'Название улицы',max_length=200, default='ул.')
+
+    name_street = models.CharField(u'Название улицы', max_length=200, default='ул.')
     id_area = models.ForeignKey(Area)
     date_create_street = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
@@ -31,14 +35,16 @@ class Street(models.Model):
     def __unicode__(self):
         return self.name_street
 
+
 class House(models.Model):
     class Meta():
         db_table = 'house'
         verbose_name = u'Дом'
         verbose_name_plural = u'Дома'
+
     street_house = models.ForeignKey(Street)
-    num_house = models.IntegerField(u'Номер дома',default=1)
-    korp_house = models.CharField(u'Корпус',max_length=15, blank=True, default='корп.')
+    num_house = models.IntegerField(u'Номер дома', default=1)
+    korp_house = models.CharField(u'Корпус', max_length=15, blank=True, default='корп.')
     date_create_house = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
 
@@ -48,12 +54,14 @@ class House(models.Model):
         else:
             return u'%s %s' % (self.street_house, self.num_house)
 
+
 class VendorModel(models.Model):
     class Meta():
         db_table = 'vendor_model'
         verbose_name = u'Производитель'
         verbose_name_plural = u'Производители'
-    vendor_name = models.CharField(u'Производитель',max_length=200)
+
+    vendor_name = models.CharField(u'Производитель', max_length=200)
     date_create_vendor = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User)
 
@@ -66,6 +74,7 @@ class SwitchModel(models.Model):
         db_table = 'switch_model'
         verbose_name = u'Модель'
         verbose_name_plural = u'Модели'
+
     vendor_model = models.ForeignKey(VendorModel)
     name_model = models.CharField(u'Название модели', max_length=200)
     num_ports_model = models.IntegerField(u'Кол-во портов', default=1)
@@ -76,12 +85,14 @@ class SwitchModel(models.Model):
     def __unicode__(self):
         return u'%s - %s' % (self.vendor_model, self.name_model)
 
+
 class Switch(models.Model):
     class Meta():
         db_table = 'switch'
         verbose_name = u'Коммутатор'
         verbose_name_plural = u'Коммутаторы'
-    name_switch = models.CharField(u'Обозначение на сети',max_length=100)
+
+    name_switch = models.CharField(u'Обозначение на сети', max_length=100)
     ipaddr_switch = models.GenericIPAddressField(default='0.0.0.0')
     pd_switch = models.CharField(max_length=100, default='пд.', blank=True, null=True)
     switch_modelid = models.ForeignKey(SwitchModel, null=True)
@@ -101,10 +112,11 @@ class Description(models.Model):
         db_table = 'description'
         verbose_name = u'Дескрипшен'
         verbose_name_plural = u'Дескрипшены'
-    switch = models.ForeignKey(Switch)
-    kv = models.CharField(u'Квартира', max_length=5)
-    pd = models.CharField(u'Подъезд', max_length=5)
-    port =  models.IntegerField(u'Порт')
-    FIO = models.CharField(u'ФИО', max_length=100)
+
+    descr_switch = models.ForeignKey(Switch)
+    descr_kv = models.CharField(u'Квартира', max_length=5)
+    descr_pd = models.CharField(u'Подъезд', max_length=5)
+    descr_port = models.IntegerField(u'Порт')
+    descr_FIO = models.CharField(u'ФИО', max_length=100)
     date_create_descr = models.DateTimeField(auto_now_add=True, null=True)
     user = models.ForeignKey(User, null=True)
