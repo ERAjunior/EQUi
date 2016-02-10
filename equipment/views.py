f0c0c27d7506a3ea499ff75edc538e5be05fe077
Dimtray <dimtray@gmail.com>
2016-02-10 12:20:26 +0300

    сделал вывод коммутаторов на глваной странице

--- equipment/views.py
+++ equipment/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from equipment.models import Switch
-from equipment.models import Area
 # Create your views here.
 
 def index(request):
@@ -10,9 +9,7 @@ def index(request):
 
 def equipment(request):
     switches = Switch.objects.all()
-    areas = Area.objects.all()
     context = {
-        'switches': switches,
-        'areas': areas
+        'switches': switches
     }
     return render(request, 'equipment/equipment.html', context,)
