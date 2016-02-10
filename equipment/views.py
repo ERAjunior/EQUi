bfd40fec5c822ee2f877e15dcebf7e10dba97674
Dimtray <dimtray@gmail.com>
2016-02-10 16:23:09 +0300

    сделал вывод коммутаторов на глваной странице

--- equipment/views.py
+++ equipment/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from equipment.models import Switch
+from equipment.models import Description
 # Create your views here.
 
 def index(request):
@@ -10,6 +11,9 @@ def index(request):
 def equipment(request):
     switches = Switch.objects.all()
     context = {
-        'switches': switches
+        'switches': switches,
     }
-    return render(request, 'equipment/equipment.html', context,)
+    return render(request, 'equipment/equipment.html', context)
+
+def switch111(request, switch_id=1):
+    return render(request, 'equipment/equipment.html', {'switch1': Switch.objects.get(id=switch_id), 'description': Description.objects.filter(descr_switch_id=switch_id)})
